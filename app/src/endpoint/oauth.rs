8e2ea7daea5b6ae73a00ac54c916a5402f859d0d
charlie <dev2023@keysafe.network>
2023-05-31 22:12:59 +0800

    fix: fix typo

--- app/src/endpoint/oauth.rs
+++ app/src/endpoint/oauth.rs
@@ -1,64 +1,63 @@
 use super::err::*;
-use std::string::String;
-use std::vec::Vec;
-use std::string::ToString;
-use http_req::{request::{RequestBuilder, Method, get, post}, tls, uri::Uri};
-use std::{net::TcpStream, convert::TryFrom};
-use serde_json::{Result, Value, Map, json, to_string};
 use crate::persistence::*;
+use http_req::{
+    request::{get, post, Method, RequestBuilder},
+    tls,
+    uri::Uri,
+};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, to_string, Map, Result, Value};
+use std::string::String;
+use std::string::ToString;
+use std::vec::Vec;
+use std::{convert::TryFrom, net::TcpStream};
 
-pub fn google_oauth2(cid: &str, cse: &str, code: &str) -> GenericResult<String>{
+pub fn google_oauth2(cid: &str, cse: &str, code: &str) -> GenericResult<String> {
     let token_req = format!(
         "code={}&client_id={}&client_secret={}grant_type=authorization_code",
-        code,
-        cid,
-        cse,
+        code, cid, cse,
     );
     let token_resp = http_post_body(
         "https://oauth2.googleapis.com/token",
         token_req,
-        Some("application/x-www-form-urlencoded")
+        Some("application/x-www-form-urlencoded"),
     );
     let v: Value = serde_json::from_str(&token_resp).unwrap();
     let token = v["access_token"].clone().to_string();
     let account_resp = http_get(
         "https://www.googleapis.com/auth/userinfo.email",
         "Authorization",
-        &format!("Bearer {}", token)
+        &format!("Bearer {}", token),
     );
     println!();
     Ok(account_resp)
 }
 
-fn twitter_oauth(code: &str) -> GenericResult<String>{
+fn twitter_oauth(code: &str) -> GenericResult<String> {
     Ok("".to_string())
 }
 
-
-fn discord_oauth(code: &str) -> GenericResult<String>{
+fn discord_oauth(code: &str) -> GenericResult<String> {
     Ok("".to_string())
 }
 
-
-fn telegram_oauth(code: &str) -> GenericResult<String>{
+fn telegram_oauth(code: &str) -> GenericResult<String> {
     Ok("".to_string())
 }
 
-
-pub fn github_oauth2(cid: &str, cse: &str, code: &str) -> GenericResult<String>{
+pub fn github_oauth2(cid: &str, cse: &str, code: &str) -> GenericResult<String> {
     let token_req = json!({
         "client_id": cid,
         "client_secret": cse,
         "code": code
     });
     let token_resp = http_post(
-        &"http://127.0.0.1:5000/access_token".to_string(), 
+        &"http://127.0.0.1:5000/access_token".to_string(),
         to_string(&token_req).unwrap(),
-        None
+        None,
     );
     if token_resp.len() == 0 {
-        return Err(GenericError::from("http error"))
+        return Err(GenericError::from("http error"));
     }
     let v: Value = serde_json::from_str(&token_resp).unwrap();
     if v["access_token"].is_null() {
@@ -68,11 +67,11 @@ pub fn github_oauth2(cid: &str, cse: &str, code: &str) -> GenericResult<String>{
     let account_resp = http_get_header(
         "https://api.github.com:443/user",
         "Authorization",
-        &format!("token {}", &token));
+        &format!("token {}", &token),
+    );
     let v2: Value = serde_json::from_str(&account_resp).unwrap();
     Ok(v2["account"].clone().to_string())
 }
-
 
 fn http_get_header(url: &str, header: &str, value: &str) -> String {
     let addr = Uri::try_from(url).unwrap();
@@ -83,9 +82,9 @@ fn http_get_header(url: &str, header: &str, value: &str) -> String {
         Ok(r) => {
             let mut stream = tls::Config::default()
                 .connect(addr.host().unwrap_or(""), r)
-                .unwrap();   
+                .unwrap();
             let mut writer = Vec::new();
-        
+
             let response = RequestBuilder::new(&addr)
                 .method(Method::GET)
                 .header("Connection", "Close")
@@ -93,22 +92,19 @@ fn http_get_header(url: &str, header: &str, value: &str) -> String {
                 .header("Accept", "application/json")
                 .send(&mut stream, &mut writer)
                 .unwrap();
-            
+
             let body = String::from_utf8_lossy(&writer);
             println!("get oauth account response is {:?}", body);
             return body.to_string();
-        },
+        }
         Err(err) => {
             println!("{:?}", err);
-            return "".to_string()
+            return "".to_string();
         }
     };
 }
 
-fn http_post_body(
-    url: &str, 
-    req: String, 
-    content:Option<&str>) -> String {
+fn http_post_body(url: &str, req: String, content: Option<&str>) -> String {
     let addr = Uri::try_from(url).unwrap();
     let conn_addr = format!("{}:{}", addr.host().unwrap(), addr.corr_port());
     let stream = TcpStream::connect(conn_addr);
@@ -118,30 +114,25 @@ fn http_post_body(
         Ok(r) => {
             let mut stream = tls::Config::default()
                 .connect(addr.host().unwrap_or(""), r)
-                .unwrap();   
+                .unwrap();
             let mut writer = Vec::new();
-        
-            let response = 
-            match content {
-                Some(c) => {
-                    RequestBuilder::new(&addr)
-                        .method(Method::POST)
-                        .body(req.as_bytes())
-                        .header("Connection", "Close")
-                        .header("Content-Type", c)
-                        .header("Accept", "application/json")
-                        .send(&mut stream, &mut writer)
-                        .unwrap()
-                },
-                None => {
-                    RequestBuilder::new(&addr)
-                        .method(Method::POST)
-                        .body(req.as_bytes())
-                        .header("Connection", "Close")
-                        .header("Accept", "application/json")
-                        .send(&mut stream, &mut writer)
-                        .unwrap()
-                },
+
+            let response = match content {
+                Some(c) => RequestBuilder::new(&addr)
+                    .method(Method::POST)
+                    .body(req.as_bytes())
+                    .header("Connection", "Close")
+                    .header("Content-Type", c)
+                    .header("Accept", "application/json")
+                    .send(&mut stream, &mut writer)
+                    .unwrap(),
+                None => RequestBuilder::new(&addr)
+                    .method(Method::POST)
+                    .body(req.as_bytes())
+                    .header("Connection", "Close")
+                    .header("Accept", "application/json")
+                    .send(&mut stream, &mut writer)
+                    .unwrap(),
             };
             if !response.status_code().is_success() {
                 println!("http post failed");
@@ -150,19 +141,15 @@ fn http_post_body(
             let body = String::from_utf8_lossy(&writer);
             println!("access token response is {}", body);
             return body.to_string();
-        },
+        }
         Err(err) => {
             println!("{:?}", err);
-            return "".to_string()
+            return "".to_string();
         }
     };
 }
 
-
-fn http_post(
-    url: &str, 
-    req: String, 
-    content:Option<&str>) -> String {
+fn http_post(url: &str, req: String, content: Option<&str>) -> String {
     println!("new http post");
     println!("url is {}", &url);
     let mut writer = Vec::new();
@@ -178,11 +165,7 @@ fn http_post(
     }
 }
 
-
-fn http_get(
-    url: &str, 
-    header_name: &str,
-    header_v: &str) -> String {
+fn http_get(url: &str, header_name: &str, header_v: &str) -> String {
     println!("new http post");
     let mut writer = Vec::new();
     let response = get(&url, &mut writer).unwrap();
@@ -197,53 +180,53 @@ fn http_get(
     }
 }
 
-
 #[derive(Deserialize, Serialize, Debug)]
 pub struct GithubOAuthReq {
     client_id: String,
     client_secret: String,
-    code: String
+    code: String,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct GithubOAuthResp {
     access_token: String,
     scope: String,
-    token_type: String
+    token_type: String,
 }
 
-
-pub fn github_oauth(
-    client_id: &str,
-    client_secret: &str,
-    code: &str
-) -> GenericResult<String> {
+pub fn github_oauth(client_id: &str, client_secret: &str, code: &str) -> GenericResult<String> {
     let http_client = reqwest::blocking::Client::new();
     let github_oauth_req = GithubOAuthReq {
         client_id: client_id.to_string(),
         client_secret: client_secret.to_string(),
-        code: code.to_string()
+        code: code.to_string(),
     };
     info!("{:?}", &github_oauth_req);
-    let res = http_client.post("https://github.com/login/oauth/access_token")
+    let res = http_client
+        .post("https://github.com/login/oauth/access_token")
         .form(&github_oauth_req)
         .header("Accept", "application/json")
-        .send().unwrap();
+        .send()
+        .unwrap();
     let token_resp = res.text().unwrap();
     info!("token response {}", &token_resp);
     let resp: GithubOAuthResp = serde_json::from_str(&token_resp)?;
     let access_token = resp.access_token;
     info!("access token {}", &access_token);
-    let user_profile = http_client.post("https://api.github.com/user")
+    let user_profile = http_client
+        .post("https://api.github.com/user")
         .header("Authorization", format!("token {}", access_token))
         .header("User-Agent", "keysafe-protocol")
-        .send().unwrap().text().unwrap();
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
     let email = parse_github_oauth_profile(user_profile)?;
     Ok(email)
 }
 
 fn parse_github_oauth_profile(oauth_result: String) -> GenericResult<String> {
-    let parsed: Value = serde_json::from_str(&oauth_result).unwrap(); 
+    let parsed: Value = serde_json::from_str(&oauth_result).unwrap();
     let obj: Map<String, Value> = parsed.as_object().unwrap().clone();
     info!("access obj {:?}", obj);
     let email = obj.get("id");
@@ -259,31 +242,28 @@ pub struct GoogleOAuthReq {
     client_secret: String,
     code: String,
     grant_type: String,
-    redirect_uri: String
+    redirect_uri: String,
 }
 
-pub fn google_oauth(
-    cid: &str, 
-    cse: &str, 
-    url: &str, 
-    code: &str
-) -> GenericResult<String>{
+pub fn google_oauth(cid: &str, cse: &str, url: &str, code: &str) -> GenericResult<String> {
     let token_req = GoogleOAuthReq {
         client_id: cid.to_string(),
         client_secret: cse.to_string(),
         code: code.to_string(),
         grant_type: "authorization_code".to_string(),
-        redirect_uri: url.to_string()
+        redirect_uri: url.to_string(),
     };
     info!("token request {:?}", &token_req);
     let http_client = reqwest::blocking::Client::new();
-    let token_resp = http_client.post("https://www.googleapis.com/oauth2/v4/token")
+    let token_resp = http_client
+        .post("https://www.googleapis.com/oauth2/v4/token")
         .form(&token_req)
         .header("Accept", "application/json")
-        .send().unwrap();
+        .send()
+        .unwrap();
     if !token_resp.status().is_success() {
         let token_resp_txt = token_resp.text()?;
-        info!("access token response is {:?}", token_resp_txt);    
+        info!("access token response is {:?}", token_resp_txt);
         return Err(GenericError::from("google oauth access token fail"));
     }
     let token_resp_txt = token_resp.text()?;
@@ -291,22 +271,23 @@ pub fn google_oauth(
     let v: Value = serde_json::from_str(&token_resp_txt).unwrap();
     let token = v["access_token"].clone().to_string();
     let bear_token = format!("Bearer {}", token);
-    let account_resp = http_client.get("https://www.googleapis.com/oauth2/v3/userinfo")
+    let account_resp = http_client
+        .get("https://www.googleapis.com/oauth2/v3/userinfo")
         .header("Authorization", bear_token)
-        .send().unwrap();
+        .send()
+        .unwrap();
     if !account_resp.status().is_success() {
         let account_resp_txt = account_resp.text()?;
         info!("oauth response is {}", account_resp_txt);
-        return Err(GenericError::from("google oauth access token fail"))
+        return Err(GenericError::from("google oauth access token fail"));
     }
     let account_resp_txt = account_resp.text()?;
     info!("oauth response is {}", account_resp_txt);
     parse_google_oauth_profile(account_resp_txt)
 }
 
-
 fn parse_google_oauth_profile(oauth_result: String) -> GenericResult<String> {
-    let parsed: Value = serde_json::from_str(&oauth_result).unwrap(); 
+    let parsed: Value = serde_json::from_str(&oauth_result).unwrap();
     let obj: Map<String, Value> = parsed.as_object().unwrap().clone();
     info!("access obj {:?}", obj);
     let email = obj.get("sub");
--- enclave/src/oauth.rs
+++ enclave/src/oauth.rs
@@ -1,48 +1,44 @@
-
-use std::string::String;
-use std::vec::Vec;
-use std::string::ToString;
-use http_req::{request::{RequestBuilder, Method, post}, tls, uri::Uri};
-use std::net::TcpStream;
-use serde_json::{Result, Value, json, to_string};
+use http_req::{
+    request::{post, Method, RequestBuilder},
+    tls,
+    uri::Uri,
+};
+use serde_json::{json, to_string, Result, Value};
 use std::collections::HashMap;
-
+use std::net::TcpStream;
+use std::string::String;
+use std::string::ToString;
+use std::vec::Vec;
 
 use super::config::OAuthClient;
 use super::err::*;
 use super::log::*;
 
-pub fn google_oauth(conf: &OAuthClient, code: &str) -> GenericResult<String>{
+pub fn google_oauth(conf: &OAuthClient, code: &str) -> GenericResult<String> {
     let token_req = format!(
-        "code={}&client_id={}&client_secret={}&grant_type={}&redirect_url={}",
-        code,
-        conf.client_id,
-        conf.client_secret,
-        "authorization_code",
-        conf.redirect_url
+        "code={}&client_id={}&client_secret={}&grant_type={}&redirect_uri={}",
+        code, conf.client_id, conf.client_secret, "authorization_code", conf.redirect_url
     );
-    let token_headers = HashMap::from([
-        ("Content-Type", "application/x-www-form-urlencoded"),
-    ]);
+    info(&format!("token req {}", &token_req));
+    let token_headers = HashMap::from([("Content-Type", "application/x-www-form-urlencoded")]);
     let token_resp = http_req(
         "https://www.googleapis.com/oauth2/v4/token",
         Method::POST,
         Some(token_req),
-        token_headers);
+        token_headers,
+    );
     let v: Value = serde_json::from_str(&token_resp?)?;
     if v["access_token"].is_null() {
         return Err(GenericError::from("github oauth failed"));
     }
     let token = v["access_token"].clone().to_string();
     let bt = format!("Bearer {}", token);
-    let user_headers = HashMap::from([
-        ("Authorization",  bt.as_str())
-    ]);
+    let user_headers = HashMap::from([("Authorization", bt.as_str())]);
     let account_resp = http_req(
         "https://www.googleapis.com/oauth2/v3/userinfo",
         Method::GET,
         None,
-        user_headers
+        user_headers,
     );
     let v2: Value = serde_json::from_str(&account_resp?)?;
     if v2["email"].is_null() {
@@ -51,7 +47,7 @@ pub fn google_oauth(conf: &OAuthClient, code: &str) -> GenericResult<String>{
     Ok(v2["email"].clone().to_string())
 }
 
-/* 
+/*
 pub fn twitter_oauth(conf: &Config, code: &str) -> GenericResult<String>{
     Ok("".to_string())
 }
@@ -67,37 +63,33 @@ pub fn telegram_oauth(conf: &Config, code: &str) -> GenericResult<String>{
 }
 */
 
-pub fn github_oauth(conf: &OAuthClient, code: &str) -> GenericResult<String>{
+pub fn github_oauth(conf: &OAuthClient, code: &str) -> GenericResult<String> {
     let token_req = format!(
         "client_id={}&client_secret={}&code={}",
-        conf.client_id,
-        conf.client_secret,
-        code);
-    let token_headers = HashMap::from([
-        ("Content-Type", "application/x-www-form-urlencoded")
-    ]);
+        conf.client_id, conf.client_secret, code
+    );
+    let token_headers = HashMap::from([("Content-Type", "application/x-www-form-urlencoded")]);
     let token_resp = http_req(
-        &"http://github.com:443/login/oauth/access_token".to_string(), 
+        &"http://github.com:443/login/oauth/access_token".to_string(),
         Method::POST,
         Some(to_string(&token_req).unwrap()),
-        token_headers
+        token_headers,
     );
     if token_resp.is_err() {
-        return Err(GenericError::from("http error"))
+        return Err(GenericError::from("http error"));
     }
     let v: Value = serde_json::from_str(&token_resp?)?;
     if v["access_token"].is_null() {
         return Err(GenericError::from("github oauth failed"));
     }
     let token = v["access_token"].clone().to_string();
-    let account_headers = HashMap::from([
-        ("Authorization", token.as_str())
-    ]);
+    let account_headers = HashMap::from([("Authorization", token.as_str())]);
     let account_resp = http_req(
         "http://api.github.com:443/user",
         Method::GET,
         None,
-        account_headers);
+        account_headers,
+    );
     if account_resp.is_err() {
         return Err(GenericError::from("github profile failed"));
     }
@@ -109,10 +101,10 @@ pub fn github_oauth(conf: &OAuthClient, code: &str) -> GenericResult<String>{
 }
 
 pub fn http_req(
-    url: &str, 
+    url: &str,
     method: Method,
-    body: Option<String>, 
-    headers: HashMap<&str, &str>
+    body: Option<String>,
+    headers: HashMap<&str, &str>,
 ) -> GenericResult<String> {
     let addr: Uri = url.parse().unwrap();
     let conn_addr = format!("{}:{}", addr.host().unwrap(), addr.corr_port());
@@ -123,7 +115,7 @@ pub fn http_req(
         Ok(r) => {
             let mut stream = tls::Config::default()
                 .connect(addr.host().unwrap_or(""), r)
-                .unwrap();   
+                .unwrap();
             let mut writer = Vec::new();
             let mut req = RequestBuilder::new(&addr);
             req.method(method)
@@ -131,7 +123,8 @@ pub fn http_req(
                 .header("Accept", "application/json");
             if body.is_some() {
                 req_body = body.unwrap();
-                req.body(req_body.as_bytes()).header("Content-Length", &req_body.as_bytes().len());
+                req.body(req_body.as_bytes())
+                    .header("Content-Length", &req_body.as_bytes().len());
             }
             for (key, val) in headers.into_iter() {
                 req.header(key, val);
@@ -147,11 +140,10 @@ pub fn http_req(
             let body = String::from_utf8_lossy(&writer);
             info(&format!("http response is {}", body));
             return Ok(body.to_string());
-        },
+        }
         Err(err) => {
             error(&format!("{:?}", err));
             return Err(GenericError::from(err));
         }
     };
 }
-
