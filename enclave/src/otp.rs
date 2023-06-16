5059309411c856b0ea20fe22ee83dd5c9b56e91a
charlie <dev2023@keysafe.network>
2023-06-16 14:19:58 +0800

    fix: try smtp with html

--- enclave/src/otp.rs
+++ enclave/src/otp.rs
@@ -61,14 +61,92 @@ impl OtpChannelClient for MailChannelClient {
             tls_write(&mut stream, c);
         }
         let m_lines = &format!(
-            "subject: DAuth Verification Code
+            r###"subject: DAuth Verification Code
     from: <{}> 
     to: <{}> 
     
-    Please use the following code to verify your account:
+    Content-Type: text/html; charset=utf-8:
     
-    {}
-    .",
+    <html lang="en-US">
+  
+        <head>
+            <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+            <title>New Account Email Template</title>
+            <meta name="description" content="New Account Email Template.">
+            <style type="text/css">
+                a:hover {{
+                    text-decoration: underline !important;
+                }}
+            </style>
+        </head>
+  
+        <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+            <!-- 100% body table -->
+            <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8" style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+                <tr>
+                    <td>
+                        <table style="background-color: #f2f3f8; max-width:670px; margin:0 auto;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;">
+                                    <a href="play.hexlink.io" title="logo" target="_blank">
+                                          <img width="300" src="https://i.postimg.cc/HkJWJSNj/hexlink.png" title="logo" alt="logo">
+                                        </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                            <td>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0" style="max-width:670px; background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:0 35px;">
+                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Verify Your Transaction
+                                        </h1>
+                                        <p style="font-size:15px; color:#455056; margin:8px 0 0; line-height:24px;">
+                                            Please use the following verification code to complete your transaction procedures.
+                                        </p>
+                                        <br>
+                                        <span style="margin:10px;"><strong>Code is valid for 5 minutes</strong>.</span><br>
+                                        <span style="display:inline-block; vertical-align:middle; margin:15px 0 15px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                        <p style="color:#455056; font-size:18px;line-height:20px; margin:0; font-weight: 500;">
+                                            <strong style="display: block;font-size: 13px; margin: 0 0 5px; color:rgba(0,0,0,.64); font-weight:normal;">Verification Code</strong>
+                                            <h2 style="font-size:2.5em; background: #1890ff;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${}</h2>
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                    <td style="text-align:center;">
+                        <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>dev.hexlink.io</strong> </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+<!--/100% body table-->
+</body>
+
+</html>
+    ."###,
             from_account, to_account, c_code
         );
         let result = tls_write(&mut stream, m_lines);
